d9ccaaac742a9448d8216f80153a2c49fd685521
JonataFreitas <jonatasaple@gmail.com>
2026-03-03 22:22:53 -0300

    feat init

---
+++ src/main/java/com/example/EstoqueFacil/EstoqueFacilApplication.java
@@ -0,0 +1,13 @@
+package com.example.EstoqueFacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueFacilApplication.class, args);
+	}
+
+}
