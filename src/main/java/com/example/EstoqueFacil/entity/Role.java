b3ecdb1866f8601bb0c6c8233cc0884e5fb34067
JonataFreitas <jonatasaple@gmail.com>
2026-03-17 13:36:12 -0300

    feat: create Role and User entities with many-to-many relationship

--- src/main/java/com/example/EstoqueFacil/entity/Role.java
+++ src/main/java/com/example/EstoqueFacil/entity/Role.java
@@ -1,4 +1,21 @@
 package com.example.EstoqueFacil.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "role")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String name;
 }
--- src/main/java/com/example/EstoqueFacil/entity/User.java
+++ src/main/java/com/example/EstoqueFacil/entity/User.java
@@ -1,4 +1,39 @@
 package com.example.EstoqueFacil.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password; // armazenar hash (BCrypt)
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
 }
