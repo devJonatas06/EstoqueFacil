7801130fe6de65bf2ea6132c1a5ed98a3dc48741
JonataFreitas <jonatasaple@gmail.com>
2026-03-17 13:34:16 -0300

    feat: create Category entity

--- src/main/java/com/example/EstoqueFacil/entity/Category.java
+++ src/main/java/com/example/EstoqueFacil/entity/Category.java
@@ -1,4 +1,26 @@
 package com.example.EstoqueFacil.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String name;
+    private String description;
+    @Column(nullable = false)
+    private boolean active;
+
 }
