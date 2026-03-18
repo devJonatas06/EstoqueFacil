249d61645c343c05c6f61c9681129a1382c61440
JonataFreitas <jonatasaple@gmail.com>
2026-03-17 21:57:01 -0300

    feat: add CategoryRepository with findByName and existsByName methods

--- src/main/java/com/example/EstoqueFacil/repository/CategoryRepository.java
+++ src/main/java/com/example/EstoqueFacil/repository/CategoryRepository.java
@@ -1,4 +1,13 @@
 package com.example.EstoqueFacil.repository;
 
-public class CategoryRepository {
+import com.example.EstoqueFacil.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
+
+    boolean existsByName(String name);
 }
