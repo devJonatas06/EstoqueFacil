fecb248b76ab81b7e86452c7c37122b231b95381
devJonata06 <jonatasaple@gmail.com>
2026-05-20 18:08:15 -0300

    retirado "C"

--- src/main/java/com/example/EstoqueFacil/config/CorrelationIdFilter.java
+++ src/main/java/com/example/EstoqueFacil/config/CorrelationIdFilter.java
@@ -10,7 +10,6 @@ import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
 import java.util.UUID;
 
